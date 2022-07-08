5c9d602103d129041e6c5175c67ac84be54c5d0c
Davis Ulrich <60760602+davisulrich@users.noreply.github.com>
2022-07-07 20:05:54 -0700

    trying onload function again

--- src/index.js
+++ src/index.js
@@ -55,6 +55,9 @@ canvas.width = 600;
 canvas.height = 625;
 
 const background = new Image();
+background.onload = function() {
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+}
 background.src = "/Alien-Invaders-3/src/images/pixel_stars.jpg";
 
 const ship1 = new Image();
@@ -279,7 +282,7 @@ function levelUp() {
 }
 
 function showStartScreen(ctx) {
-  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+//   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
   ctx.drawImage(ship1, 95, canvas.height - 157, 75, 75);
   ctx.drawImage(ship2, 265, canvas.height - 160, 75, 75);
   ctx.drawImage(ship3, 430, canvas.height - 160, 75, 75);
